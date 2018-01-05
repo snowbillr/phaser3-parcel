7e54f406f40c2004403782f3a9a3ede3eaa9f25a
Bill Reed <billnreed@gmail.com>
2018-01-05 15:42:04 -0500

    parcel works

---
+++ README.md
@@ -0,0 +1,5 @@
+Phaser 3 + Parcel
+
+1. `yarn install`
+1. `yarn dev`
+1. Visit `localhost:1234`
---
+++ index.js
@@ -0,0 +1,17 @@
+import 'phaser';
+
+const config = {
+  type: Phaser.WEBGL,
+  parent: 'game',
+  width: 640,
+  height: 480,
+  scene: {
+      create: create
+  }
+};
+
+const game = new Phaser.Game(config);
+
+function create() {
+  const text = this.add.text(100, 100, 'Phaser + Parcel');
+}
