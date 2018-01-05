bc5163e828b0270ac40a2769891a7b6289bd49df
Bill Reed <billnreed@gmail.com>
2018-01-05 15:59:02 -0500

    hmr works

--- index.js
+++ index.js
@@ -10,8 +10,18 @@ const config = {
   }
 };
 
+const gameEl = document.querySelector(`#${config.parent}`);
 const game = new Phaser.Game(config);
 
 function create() {
   const text = this.add.text(100, 100, 'Phaser + Parcel');
+}
+
+if (module.hot) {
+  module.hot.accept(() => {
+    while (gameEl.firstChild) {
+      gameEl.removeChild(gameEl.firstChild);
+    }
+    game.boot();
+  });
 }
